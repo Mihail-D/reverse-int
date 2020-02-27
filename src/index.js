c638e1c1eb4c611e0e0915ac9f594f82d43b8306
Mihail-D <nuby.4register@gmail.com>
2020-02-27 22:21:49 +0300

    feat: complete 'Reverse Int' task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  Math.abs();
-}
+module.exports = function reverse(n) {
+    n = Math.abs(n)
+        .toString()
+        .split("")
+        .reverse()
+        .join("");
+    return parseInt(n);
+};
